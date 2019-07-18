18a64c2a499d38f99f517621df75b6b76b8f53b2
Jeremy Likness <jeremy@jeremylikness.com>
2019-07-18 14:16:30 -0700

    Add project files.

---
+++ SliderExample/wwwroot/slider.js
@@ -0,0 +1,13 @@
+﻿window.slider = {
+    inpElem: { value: '' },
+    outputElem: { innerText: '' },
+    onslide: () => {
+        window.slider.outputElem.innerText =
+            window.slider.inpElem.value;
+    },
+    attach: (inp, outp) => {
+        window.slider.inpElem = inp;
+        window.slider.outputElem = outp;
+        inp.addEventListener("input", window.slider.onslide);
+    }
+};
